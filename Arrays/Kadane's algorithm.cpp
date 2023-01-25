4588b48f51dab64c3f0d09d2566f50e648763289
JaeAeich <javedh@iitbhilai.ac.in>
2023-01-25 12:49:33 +0530

    updated kadanes

--- Arrays/Kadane's algorithm.cpp
+++ Arrays/Kadane's algorithm.cpp
@@ -2,18 +2,27 @@
 using namespace std;
 
 /* Kadane's algorithm , Time complexity: O(N), Space Complexity: O(1).
+This algorithm is used to find the maximum contiguous subarray with greates sum in linear time,
+e.g: [1,-2,-1,2,-1,2,-1,-4]==>max sum subarray is [2,-1,2] .
+
+Working:
+        This algorithm works by simply adding the element till you get a negative sum, once a negative sum is obtained we can be sure this won't 
+        previous subarray wont be part of max subarray, in such case we store prev sum and start another subarray from next idx, and if we get
+        a sum greater than the previous, we update that. 
+
 
 NOTE: There exists another variation of problem where all elements of vectors are negative in that case,
-      below algo will fail. In such case return the least greates element of array using lenear search.
+      below algo will fail. In such case return the least greatest element of array using lenear search.
 
-
+      e.g v=[-1,-2,-24,-2,-23,-122] => max sum subarray is [-1].
+      This will just be a simple linear search for greatest num.
 */
 
 int maxSumSubarray(vector<int> &v)
 {
     int n = v.size();
     int sum = 0, maxSum = INT_MIN;
-    // finding a prefix sum that not negative.
+    // finding a prefix sum thats not negative.
     for (int i = 0; i < n; i++)
     {
         sum += v[i];
